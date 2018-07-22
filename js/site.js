2cef7c9824fb7789833c5a7149faf305c86b7efe
Krishnan Ramalingam <kramali4@ford.com>
2018-07-21 23:08:45 -0400

    first commit

---
+++ js/site.js
@@ -0,0 +1,26 @@
+function gmtl(lhs, rhs, subject) {
+ document.write("<a property=\"schema:email\" class=\"email\" href=\"mailto");
+ document.write(":" + lhs + "@");
+ document.write(rhs + "?subject=" + subject + " \">" + lhs + "@" + rhs + "<\/a>");
+};
+
+var skillsdata = (function() {
+ var skills = null;
+ $.ajax({
+  'async': false,
+  'global': false,
+  'url': "js/skills.json",
+  'dataType': "json",
+  'success': function(data) {
+   skills = data;
+  }
+ });
+ return skills;
+})();
+
+var skills = JSONTree.create(skillsdata);
+
+function expColToggle(x) {
+ x.classList.toggle("expst");
+ x.classList.toggle("colst");
+}
